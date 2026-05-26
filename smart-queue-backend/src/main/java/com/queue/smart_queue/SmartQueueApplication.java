464fb06dd3f6e367f89f0de11b7f28132c4d245f
Felipe Marques <feelipemarquees@gmail.com>
2026-05-26 12:06:43 -0300

    initial commit

---
+++ smart-queue-backend/src/main/java/com/queue/smart_queue/SmartQueueApplication.java
@@ -0,0 +1,13 @@
+package com.queue.smart_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartQueueApplication.class, args);
+	}
+
+}
---
+++ smart-queue-backend/src/test/java/com/queue/smart_queue/SmartQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.smart_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
