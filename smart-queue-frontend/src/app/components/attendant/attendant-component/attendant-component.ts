ecce067c86c8d53295ef27468b234c998c1d02e5
Felipe Marques <feelipemarquees@gmail.com>
2026-05-31 15:00:40 -0300

    feat: finalize components basics features (CSS by Claude)

--- smart-queue-frontend/src/app/components/attendant/attendant-component/attendant-component.ts
+++ smart-queue-frontend/src/app/components/attendant/attendant-component/attendant-component.ts
@@ -17,7 +17,9 @@ export class AttendantComponent {
   counterId: string = '';
   counterStatus: boolean = false;
   tickets: TicketResponse[] = [];
-
+  isInService: boolean = false;
+  currentTicket: string = '';
+  errorMessage = '';
 
   constructor(counterService: CounterService, ticketService: TicketService){
     this.counterService = counterService;
@@ -27,13 +29,13 @@ export class AttendantComponent {
   ngOnInit(){
     if(localStorage.getItem("counterId")){
       this.counterId = localStorage.getItem("counterId")!;
+      this.currentTicket = localStorage.getItem("currentTicket")!;
+      this.isInService = localStorage.getItem("isInService")?.match("true") ? true : false;
       this.counterService.getCurrentStatus(this.counterId).subscribe(response => { this.counterStatus = response});
     }else{
       this.counterId = '';
     }
-    this.ticketService.getAllTickets().subscribe(response => {
-      this.tickets = response;
-    });
+    this.refreshTickets();
   }
 
   openCounter(){
@@ -45,13 +47,40 @@ export class AttendantComponent {
       });
   }
 
-  changeCounterStatus() { //Improve: The counterStatus needs to be updated when button is clicked!!
-    this.counterService.changeCounterStatus(this.counterId, !this.counterStatus).subscribe();
-    this.counterService.getCurrentStatus(this.counterId).subscribe(response => { this.counterStatus = response});
+  changeCounterStatus() { 
+    this.counterService.changeCounterStatus(this.counterId, !this.counterStatus).subscribe(r =>{
+      this.counterService.getCurrentStatus(this.counterId).subscribe(response => { this.counterStatus = response});
+    }); 
   }
 
+  refreshTickets() {
+    this.ticketService.getAllTickets().subscribe(response => {
+      this.tickets = response;
+    });
+  }
 
+  callNextTicket() {
+    this.counterService.callNextTicket(this.counterId).subscribe({next: response => {
+      this.currentTicket = response.number;
+      localStorage.setItem("currentTicket", this.currentTicket);
+      this.isInService = true;
+      localStorage.setItem("isInService", "true");
+      this.refreshTickets();
+    },
+    error: err => {
+      this.errorMessage = err.error.message;
+      setTimeout(() => this.errorMessage = '', 1000);
+    }});
+  }
 
-
-
+  finishCurrentTicket(){
+    this.counterService.finishTicket(this.counterId, this.currentTicket).subscribe({next: response =>{
+      this.currentTicket = '';
+      localStorage.removeItem("currentTicket");
+      this.isInService = false;
+      localStorage.removeItem("isInService");
+      this.errorMessage = "Success!";
+      setTimeout(() => this.errorMessage = '', 1000);
+    }});
+  }
 }
